ff94d434eabbd4b3d3c2cea62b14f107cf3b068d
ctrangel <ctrangel@pointpark.edu>
2024-01-13 23:02:57 -0500

    ADD: night mode toggle

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-
-
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   const links = document.querySelectorAll('a[href^="#"]');
-  
-  links.forEach(link => {
-    link.addEventListener('click', smoothScroll);
+
+  links.forEach((link) => {
+    link.addEventListener("click", smoothScroll);
   });
-  
+
   function smoothScroll(e) {
     e.preventDefault();
-    
-    const targetId = this.getAttribute('href').substring(1);
+
+    const targetId = this.getAttribute("href").substring(1);
     const targetElement = document.getElementById(targetId);
-    
+
     window.scrollTo({
       top: targetElement.offsetTop,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
   }
 });
+
+function toggleNightMode() {
+  document.body.classList.toggle("night-mode");
+}
+
+document
+  .getElementById("night-mode-toggle")
+  .addEventListener("click", toggleNightMode);
