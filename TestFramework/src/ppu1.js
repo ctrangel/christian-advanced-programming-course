fa7424f6fa38189c9952fa5c8962f32a0c4f9c06
ctrangel <122327961+ctrangel@users.noreply.github.com>
2024-03-25 14:36:56 -0400

    non-working version

---
+++ TestFramework/src/ppu1.js
@@ -0,0 +1,22 @@
+class Bison {
+  add(a, b) {
+    return a + b;
+  }
+
+  subtract(a, b) {
+    return a - b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+
+  divide(a, b) {
+    if (b === 0) {
+      throw new Error("Cannot divide by the zero that is illegal");
+    }
+    return a / b;
+  }
+}
+
+export default Bison;
